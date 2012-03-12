41d0fef6003cd38e8d3df4a822ede79539aad3a6
Zonta <mangemonpain@x-dns.org>
2012-03-11 22:55:08 -0400

    Next version
    Email Solved
    
    problem?@x-dns.org

--- PearlXP/src/PearlXP/PearlXP.java
+++ PearlXP/src/PearlXP/PearlXP.java
@@ -15,24 +15,8 @@ public class PearlXP extends org.bukkit.plugin.java.JavaPlugin {
 	
 	public void onEnable() {
 		logger = Logger.getLogger("Minecraft");
-
 		server = this.getServer();
-		//getConfig().options().copyDefaults(true);
-        //saveConfig();
 		
-		/*try {
-			if (!(new java.io.File(this.getDataFolder(), "config.yml").exists())) {
-				java.io.FileOutputStream writer = new java.io.FileOutputStream(new java.io.File(getDataFolder(), "config.yml"));
-				java.io.InputStream out = PearlXP.class.getResourceAsStream("config.yml");
-				byte[] linebuffer = new byte[4096];
-				int lineLength = 0;
-				while((lineLength = out.read(linebuffer)) > 0) {
-					writer.write(linebuffer, 0, lineLength);
-				}
-				writer.close();
-			}
-		} catch (java.io.IOException e) {}*/
-		//if (!(new java.io.File(this.getDataFolder(),"config.yml").exists())) {saveDefaultConfig();}
 		if(YamlConfiguration.loadConfiguration(new File(getDataFolder(), "config.yml")).getInt("configversion",0) < 1) {saveResource("config.yml",true); reloadConfig();}
 		else{getConfig().options().copyDefaults(true);}
 	
