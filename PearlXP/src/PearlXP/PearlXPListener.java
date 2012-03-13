77639441e30d36a1ac15245219d43b8dad74a86e
Zonta <mangemonpain@x-dns.org>
2012-03-13 12:44:59 -0400

    fix bugs and first implementation of damage based soul gem.

--- PearlXP/src/PearlXP/PearlXPListener.java
+++ PearlXP/src/PearlXP/PearlXPListener.java
@@ -5,83 +5,92 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.inventory.ItemStack;
 
 public class PearlXPListener implements org.bukkit.event.Listener {
-
+	
 	public PearlXP instance;
+	
+	
+	private static String itemName = "pearl";
 
 	public PearlXPListener(PearlXP owner) {
 		instance = owner;
-	}
 
+	}
+	
+	
+	
 	@EventHandler
 	public void onPlayerInteract(org.bukkit.event.player.PlayerInteractEvent event) {
 		ItemStack item = event.getItem();
-		String itemname = "pearl";
+		int itemId = instance.getConfig().getInt("itemid");
 		
-		int level;
-		int levels;
+		int maxLevel;
+		int playerXp;
 		
 		Player ply = event.getPlayer();
-		XPEditor xpeditor = new XPEditor(ply);
 		
-		if (item != null
-				&& item.getTypeId() == instance.getConfig().getInt("itemid")) {
+		if (item != null && item.getTypeId() == itemId) {
 			
-			if (item.getTypeId() != 368) {
-				itemname = "item";
+			
+			if (item.getDurability() > 0 && event.getAction() == org.bukkit.event.block.Action.RIGHT_CLICK_AIR
+					|| event.getAction() == org.bukkit.event.block.Action.RIGHT_CLICK_BLOCK) {
+				
+				ply.sendMessage("This " + itemName + " is imbued with "
+						+ item.getDurability() + " XP!");
+				//event.setCancelled(true);
+				
 			}
-
-			if (item.getDurability() >= 1
-					&& (event.getAction() == org.bukkit.event.block.Action.RIGHT_CLICK_BLOCK 
-					|| event.getAction() == org.bukkit.event.block.Action.RIGHT_CLICK_AIR)) {
-				
-				level = item.getDurability();
-
-				ply.sendMessage("This " + itemname + " is imbued with "
-						+ XPEditor.xpLookup[level] + "xp (level 0->" + level
-						+ ")!");
-				event.setCancelled(true);
-				
-			} else if (event.getAction() == org.bukkit.event.block.Action.RIGHT_CLICK_BLOCK
-					&& item.getTypeId() == 368) {
-				
-				ply.sendMessage("Throwing a pearl at your feet would hurt!");
-				event.setCancelled(true);
-				
-			} else if (event.getAction() == org.bukkit.event.block.Action.LEFT_CLICK_AIR
+			
+			if (event.getAction() == org.bukkit.event.block.Action.LEFT_CLICK_AIR
 					|| event.getAction() == org.bukkit.event.block.Action.LEFT_CLICK_BLOCK) {
 				
 				if (item.getAmount() > 1) {
-					ply.sendMessage("To store experience in the " + itemname
+					ply.sendMessage("To store experience in the " + itemName
 							+ ", please unstack them!");
 					return;
 				}
 				
-				
-				xpeditor.recalcTotalExp();
-				levels = xpeditor.getLevel();
 
-				if (item.getDurability() >= 1) {
-					level = item.getDurability();
+				if (item.getDurability() > 0) {
+					
+					ply.giveExp(item.getDurability());
+					
+					ply.sendMessage("Restoring " + item.getDurability() + " XP! You now have " 
+								+ ply.getTotalExperience() + " XP!");
+					
 					item.setDurability((short) 0);
-					xpeditor.giveExp(XPEditor.xpLookup[level]);
-					ply.sendMessage("Restoring " + XPEditor.xpLookup[level]
-							+ "xp (level 0->" + level + ")!");
 					
-				} else if (levels == 0) {
+				} else { // the item is empty
 					
-					if (levels > instance.getConfig().getInt("maxlevel")) {
-						levels = instance.getConfig().getInt("maxlevel");
+					maxLevel = instance.getConfig().getInt("maxlevel");
+					
+					if (ply.getTotalExperience() > maxLevel) {
+						
+						// remove only the maximum xp
+						playerXp = ply.getTotalExperience() - maxLevel;
+						item.setDurability( (short) maxLevel);
+						ply.setTotalExperience(0);
+						ply.setLevel(0);
+						ply.giveExp(playerXp);
+						
+						ply.sendMessage("Imbued this " + itemName + " with "
+								+ item.getDurability() + " XP! You have " + ply.getTotalExperience() + "XP left!" );
+					} else {
+						
+						//Get Player XP and Set it to the Item Durability
+						item.setDurability( (short) ply.getTotalExperience()); 
+
+						ply.setTotalExperience(0);
+						ply.setLevel(0);
+						
+						ply.sendMessage("Imbued this " + itemName + " with "
+								+ item.getDurability() + " XP!");
 					}
-					
-					xpeditor.takeExp(XPEditor.xpLookup[levels]);
-					item.setDurability((short) levels);
-					ply.sendMessage("Imbued this " + itemname + " with "
-							+ XPEditor.xpLookup[levels] + "xp (level 0->"
-							+ levels + ")!");
 				}
+				
 			}
+		
 		}
 		
-	}
+	} //onPlayerInteract
 	
-}
+} //class
