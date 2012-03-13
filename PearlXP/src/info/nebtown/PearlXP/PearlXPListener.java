8848529c542e59c92ca5c3e1aabceac3cc63c7b9
marex <noware22@gmail.com>
2012-03-13 17:20:15 -0400

    change storage system to store as enchentments (again !)

--- PearlXP/src/info/nebtown/PearlXP/PearlXPListener.java
+++ PearlXP/src/info/nebtown/PearlXP/PearlXPListener.java
@@ -2,6 +2,7 @@ package info.nebtown.PearlXP;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Effect;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,9 +26,10 @@ public class PearlXPListener implements Listener {
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		ItemStack item = event.getItem();
+		Enchantment enchantment = Enchantment.OXYGEN;
 		
 		int maxLevel = PearlXP.getMaxLevel();
-		int playerXp;
+		int xpStored = item.getEnchantmentLevel(enchantment);
 		
 		Player player = event.getPlayer();
 		
@@ -39,13 +41,13 @@ public class PearlXPListener implements Listener {
 				
 				
 				
-				if (item.getDurability() != 0) {
+				if (item.getEnchantmentLevel(enchantment) != 0) {
 					// the item have stored XP
 					
 					event.setCancelled(true); // keep the item !
 					
 					sendInfo("This " + itemName + " is imbued with "
-							+ item.getDurability() + " XP!", player);
+							+ xpStored + " XP!", player);
 					
 				} else if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 					
@@ -66,41 +68,40 @@ public class PearlXPListener implements Listener {
 				}
 				
 
-				if (item.getDurability() > 0) {
+				if (xpStored > 0) {
 					
-					player.giveExp(item.getDurability());
+					player.giveExp(xpStored);
 					
-					sendInfo("+Restoring " + item.getDurability() + " XP! You now have " 
+					sendInfo("+Restoring " + xpStored + " XP! You now have " 
 								+ player.getTotalExperience() + " XP!", player);
 					
-					item.setDurability((short) 0);
+					sendInfo("Enchent lvl : " + item.getEnchantmentLevel(enchantment), player);
+					item.removeEnchantment(enchantment);
 					
 				} else { // the item is empty
 					
+					// Visual and sound effects
 					player.getWorld().playEffect(player.getEyeLocation(), Effect.ENDER_SIGNAL, 0);
 					player.playEffect(player.getEyeLocation(), Effect.EXTINGUISH, 0);
 					
 					if (player.getTotalExperience() > maxLevel) {
 						
-						// remove only the maximum xp
-						playerXp = player.getTotalExperience() - maxLevel;
-						item.setDurability((short) maxLevel);
-						player.setTotalExperience(0);
-						player.setLevel(0);
-						player.giveExp(playerXp);
+						
+						item.addUnsafeEnchantment(enchantment, maxLevel);
+						
+						removePlayerXp(maxLevel, player);
 						
 						sendInfo("-Imbued this " + itemName + " with "
-								+ item.getDurability() + " XP! You have " + player.getTotalExperience() + "XP left!", player);
+								+ maxLevel + " XP! You have " + player.getTotalExperience() + "XP left!", player);
 					} else {
 						
-						//Get Player XP and Set it to the Item Durability
-						item.setDurability((short) player.getTotalExperience()); 
+						//Get Player XP and store it into the item
+						item.addUnsafeEnchantment(enchantment, player.getTotalExperience()); 
 
-						player.setTotalExperience(0);
-						player.setLevel(0);
+						removePlayerXp(player.getTotalExperience(), player);
 						
 						sendInfo("Imbued this " + itemName + " with "
-								+ item.getDurability() + " XP!", player);
+								+ item.getEnchantmentLevel(enchantment) + " XP!", player);
 					}
 				}
 				
@@ -117,6 +118,22 @@ public class PearlXPListener implements Listener {
 	 */
 	private void sendInfo(String s, Player p) {
 		p.sendMessage(textColor + s);
+	}
+	
+	/**
+	 * Remove a number of XP from a given player
+	 * @param xp the XP to remove
+	 * @param p player
+	 */
+	private void removePlayerXp(int xp, Player p) {
+		int currentXp = p.getTotalExperience();
+		
+		// Reset level to fix update bug
+		p.setTotalExperience(0);
+		p.setLevel(0);
+		
+		p.giveExp(currentXp - xp);
+		
 	}
 	
 } //class
