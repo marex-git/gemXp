e4b75182be717fa8c7888d2504ac49f671fe4b38
marex <noware22@gmail.com>
2012-03-14 00:09:13 -0400

    first implementation of a working pearl in stacks

--- PearlXP/src/info/nebtown/PearlXP/PearlXPListener.java
+++ PearlXP/src/info/nebtown/PearlXP/PearlXPListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.event.block.Action;
 
 public class PearlXPListener implements Listener {
@@ -16,32 +17,33 @@ public class PearlXPListener implements Listener {
 	
 	private static String itemName = "pearl";
 	private static ChatColor textColor = ChatColor.BLUE;
+	private static Enchantment enchantment = Enchantment.OXYGEN;
 	
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		ItemStack item = event.getItem();
-		Enchantment enchantment = Enchantment.OXYGEN;
 		
-		int maxLevel = PearlXP.getMaxLevel();
-		int xpStored = item.getEnchantmentLevel(enchantment);
+		int xpToStore = 0;
+		String storeMsg = "-Imbued this " + itemName + " with ";
 		
 		Player player = event.getPlayer();
+		PlayerInventory inventory = player.getInventory();
 		
 		if (item != null && item.getTypeId() == PearlXP.getItemId()) {
 			
 			
-			if (item.getAmount() == 1 && (event.getAction() == Action.RIGHT_CLICK_AIR
-					|| event.getAction() == Action.RIGHT_CLICK_BLOCK)) {
+			if (event.getAction() == Action.RIGHT_CLICK_AIR
+					|| event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 				
 				
 				
-				if (item.getEnchantmentLevel(enchantment) != 0) {
+				if (item.containsEnchantment(enchantment)) {
 					// the item have stored XP
 					
 					event.setCancelled(true); // keep the item !
 					
 					sendInfo("This " + itemName + " is imbued with "
-							+ xpStored + " XP!", player);
+							+ getStoredXp(item) + " XP!", player);
 					
 				} else if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 					
@@ -55,47 +57,49 @@ public class PearlXPListener implements Listener {
 			} else if (event.getAction() == Action.LEFT_CLICK_AIR
 						|| event.getAction() == Action.LEFT_CLICK_BLOCK) {
 				
-				if (item.getAmount() > 1) {
-					sendInfo("To store experience in the " + itemName
-							+ ", please unstack them!", player);
-					return;
-				}
+				/*if (item.getAmount() > 1) {
+					
+					item = unStack(item, inventory);
+				}*/
 				
-
-				if (xpStored > 0) {
+				if (item.containsEnchantment(enchantment)) {
+					// The item is alone and has stored XP
 					
-					player.giveExp(xpStored);
+					if (item.getAmount() > 1) item = unStack(item, inventory);
 					
-					sendInfo("+Restoring " + xpStored + " XP! You now have " 
+					player.giveExp(getStoredXp(item));
+					
+					sendInfo("+Restoring " + getStoredXp(item) + " XP! You now have " 
 								+ player.getTotalExperience() + " XP!", player);
+
+					// Remove all Stored xp
+					setStoredXp(0, item);
 					
-					item.removeEnchantment(enchantment);
 					
-				} else { // the item is empty
+				} else if (player.getTotalExperience() > 0) { 
+					// the item is empty and the player has xp
 					
+					if (item.getAmount() > 1) item = unStack(item, inventory);
+					
+					if (player.getTotalExperience() > PearlXP.getMaxLevel()) {
+						
+						xpToStore = PearlXP.getMaxLevel();
+						storeMsg +=  xpToStore + " XP! " + player.getTotalExperience() + "XP left!";
+					} else {
+						
+						xpToStore = player.getTotalExperience();
+						storeMsg += xpToStore + " XP!";
+					}
+					
+					setStoredXp(xpToStore, item);
+					removePlayerXp(xpToStore, player);
+					
+					// Friendly message !
+					sendInfo(storeMsg, player);
+
 					// Visual and sound effects
 					player.getWorld().playEffect(player.getEyeLocation(), Effect.ENDER_SIGNAL, 0);
 					player.playEffect(player.getEyeLocation(), Effect.EXTINGUISH, 0);
-					
-					if (player.getTotalExperience() > maxLevel) {
-						
-						
-						item.addUnsafeEnchantment(enchantment, maxLevel);
-						
-						removePlayerXp(maxLevel, player);
-						
-						sendInfo("-Imbued this " + itemName + " with "
-								+ maxLevel + " XP! You have " + player.getTotalExperience() + "XP left!", player);
-					} else {
-						
-						//Get Player XP and store it into the item
-						item.addUnsafeEnchantment(enchantment, player.getTotalExperience()); 
-
-						removePlayerXp(player.getTotalExperience(), player);
-						
-						sendInfo("Imbued this " + itemName + " with "
-								+ item.getEnchantmentLevel(enchantment) + " XP!", player);
-					}
 				}
 				
 			}
@@ -128,6 +132,56 @@ public class PearlXPListener implements Listener {
 		
 		p.giveExp(currentXp - xp);
 		
+	}
+	
+	/**
+	 * Unstack one item in the inv inventory.
+	 * @param stack ItemStack to remove one item
+	 * @param inv inventory
+	 */
+	private ItemStack unStack(ItemStack stack, PlayerInventory inv) {
+		int slot = inv.firstEmpty();
+		ItemStack newItem = stack.clone();
+		
+		// Only create one item...
+		newItem.setAmount(1);
+		
+		stack.setAmount(stack.getAmount() - 1);
+		inv.setItem(slot, newItem);
+		
+		return inv.getItem(slot);
+	}
+	
+	/**
+	 * Return the amount of experience points stored in the item
+	 * @param item
+	 * @return xp experience points
+	 */
+	private int getStoredXp(ItemStack item) {
+		int xp = 0;
+		
+		if (item.containsEnchantment(enchantment))
+			xp = item.getEnchantmentLevel(enchantment);
+
+		return xp;
+	}
+	
+	/**
+	 * Set the amount of stored experience points in the item i to xp
+	 * @param xp new stored experience points
+	 * @param item
+	 */
+	private void setStoredXp(int xp, ItemStack item) {
+		// check for overflow
+		if (xp > PearlXP.getMaxLevel()) {
+			xp = PearlXP.getMaxLevel();
+		}
+		
+		if (xp == 0) {
+			item.removeEnchantment(enchantment);
+		} else {
+			item.addUnsafeEnchantment(enchantment, xp);
+		}
 	}
 	
 } //class
